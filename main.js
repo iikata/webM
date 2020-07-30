3ec2f89050441ecfcb8548dbe622268fd05d7740
mba <johndoe@example.com>
2020-07-31 07:41:25 +0900

    改修

--- main.js
+++ main.js
@@ -4,3 +4,14 @@ function showSum() {
     var sum = num1 + num2;
     alert(sum);
 }
+
+  $(function(){
+    $('a[href^="#"]').click(function(){
+      var speed = 500;
+      var href= $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top;
+      $("html, body").animate({scrollTop:position}, speed, "swing");
+      return false;
+    });
+  });
